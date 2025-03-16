dd85348a845c0c54222af313ca52ca78043054f3
superbAnna <157947560+superbAnna@users.noreply.github.com>
2025-03-17 02:46:58 +0800

    cs224n作业

--- 吴恩达深度学习/lecture1_week2/lecture1_week2.py
+++ 吴恩达深度学习/lecture1_week2/lecture1_week2.py
@@ -101,9 +101,20 @@ print("====================测试model====================")
 # 这里加载的是真实的数据
 d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
 
-costs = np.squeeze(d['costs'])
-plt.plot(costs)
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+for i in learning_rates:
+    print ("learning rate is: " + str(i))
+    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1500, learning_rate = i, print_cost = False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
+
+for i in learning_rates:
+    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+
 plt.ylabel('cost')
-plt.xlabel('iterations (per hundreds)')
-plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.xlabel('iterations')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
 plt.show()
